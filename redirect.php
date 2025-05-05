dfffe216485980b7532d9d46c1beea8e3431faaf
Lina Brown <brownli2@appstate.edu>
2025-05-05 14:33:38 -0400

    removing require config.php

--- redirect.php
+++ redirect.php
@@ -22,7 +22,6 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-require_once('../../config.php');
 global $DB;
 
 $id = required_param('id', PARAM_ALPHANUM); // Link id from the table.
