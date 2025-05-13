ca99baf8b91bda2e6804e7db5bfc6e44645ceada
Lina Brown <brownli2@appstate.edu>
2025-05-13 12:12:00 -0400

    remove leading slash

--- settings.php
+++ settings.php
@@ -30,11 +30,11 @@ if ($hassiteconfig) {
     $settings = new admin_settingpage('local_onboarding_settings', get_string('pluginname', 'local_onboarding'));
 
     if ($ADMIN->fulltree) {
-        $settings->add(new \local_onboarding\admin_setting_confightmleditor_with_validation('onboarding/welcometeacher',
+        $settings->add(new local_onboarding\admin_setting_confightmleditor_with_validation('onboarding/welcometeacher',
             get_string('welcometeacher', 'local_onboarding'), get_string('welcometeacher_desc', 'local_onboarding'), '', PARAM_RAW));
-        $settings->add(new \local_onboarding\admin_setting_confightmleditor_with_validation('onboarding/welcomestudent', get_string('welcomestudent', 'local_onboarding'),
+        $settings->add(new local_onboarding\admin_setting_confightmleditor_with_validation('onboarding/welcomestudent', get_string('welcomestudent', 'local_onboarding'),
             get_string('welcomestudent_desc', 'local_onboarding'), '', PARAM_RAW));
-        $settings->add(new \local_onboarding\admin_setting_confightmleditor_with_validation('onboarding/lowuseteacher',
+        $settings->add(new local_onboarding\admin_setting_confightmleditor_with_validation('onboarding/lowuseteacher',
             get_string('lowuseteacher', 'local_onboarding'), get_string('lowuseteacher_desc', 'local_onboarding'), '', PARAM_RAW));
     }
 
